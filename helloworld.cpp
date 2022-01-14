4623461f53bae1c97bd4e946bb7eef46b48b0f51
X-YiXuan <95702931+X-YiXuan@users.noreply.github.com>
2022-01-14 15:11:17 +0800

    Add files via upload

---
+++ helloworld.cpp
@@ -0,0 +1,13 @@
+
+#include <iostream>
+
+/**
+ * 程序执行的入口点。
+ */
+int main() {
+    // 在标准输出中打印 "Hello, world!"
+    std::cout << "Hello, world!" << std::endl;
+}
+
+// 此文件编译运行将输出 "Hello, world!"。
+// 按下 F6 后，您将在弹出的窗口中看到这一行字。
